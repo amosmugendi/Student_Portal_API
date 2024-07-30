c4e02c822d052b01f6e273241047d3e2f41fa5c2
jayson <jaysonshammah3@gmail.com>
2024-07-30 12:35:16 +0300

    students ft jayson

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 from auth import auth_bp, bcrypt, jwt
 from models import db
-# from student import student_bp
-# from admin import admin_bp
+from students import students_bp  # Import the students blueprint
 from datetime import timedelta
 
 app = Flask(__name__)
@@ -25,8 +24,7 @@ migrate = Migrate(app, db)
 
 # Register blueprints
 app.register_blueprint(auth_bp)
-# app.register_blueprint(student_bp)
-# app.register_blueprint(admin_bp)
+app.register_blueprint(students_bp)  # Register the students blueprint
 
 # Basic route
 @app.route("/")
