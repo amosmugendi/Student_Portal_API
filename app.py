a15e3b4e40bdefec1f3ddc79329cf0e0266de505
amosmugendi <amosmugendi07@gmail.com>
2024-08-15 16:37:31 +0300

    reset acess_token

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from flask_mail import Mail
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///moringa_students_portal.db'
 app.config['SECRET_KEY'] = 'MoringaStudents'
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=30)
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=3)
 app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=30)
 
 CORS(app)
